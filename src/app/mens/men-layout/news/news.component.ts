44ffdf6734518b765a3b608cca9c8dee22ff110b
Tobi17890 <phengeangzer@gmail.com>
2023-12-12 23:02:06 +0700

    Update router links and dropdown functionality

--- src/app/mens/men-layout/news/news.component.ts
+++ src/app/mens/men-layout/news/news.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
+  ngOnInit(): void {
+    this.setupOnClickEvent();
+  }
 
+  myFunction(dropdown: HTMLDivElement) {
+    dropdown.classList.toggle('show');
+  }
+
+  setupOnClickEvent(): void {
+    window.onclick = (event: any) => {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
+  }
 }
